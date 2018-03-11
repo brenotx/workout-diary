908f1fa1f00cddcb165dc5a104e641501b8a62c0
Breno Teixeira <brenotx@gmail.com>
2018-03-10 23:04:09 -0300

    Changing css 'px' unit to 'em'

--- src/containers/Auth/AuthBox.js
+++ src/containers/Auth/AuthBox.js
@@ -3,19 +3,19 @@ import styled from "styled-components";
 
 const AuthBox = styled.div`
     position: relative;
-    margin: 15% auto;
-    width: 300px;
-    height: 400px;
+    margin: 10% auto;
+    width: 19em;
+    height: 25em;
     padding: 2em;
     background: #fff;
-    box-shadow: 2px 2px 5px 1px rgba(0, 0, 0, 0.2);
-    border-radius: 3px;
+    box-shadow: 0.125em 0.125em 0.3125em 0.0625em rgba(0, 0, 0, 0.2);
+    border-radius: 0.1875em;
 
     & > img {
-        height: 70px;
-        width: 70px;
+        height: 4.375em;
+        width: 4.375em;
         display: block;
-        margin: 10px auto 40px;
+        margin: 0.625em auto 2.5em;
     }
 `;
 
